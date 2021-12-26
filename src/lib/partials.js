5898b349691cc8faccc027acc2112e445ed3af33
Tobias Heinrich <tabbe123@googlemail.com>
2021-12-26 22:15:49 +0100

    robust loading of partials.json

--- src/lib/partials.js
+++ src/lib/partials.js
@@ -1,5 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+const stripJsonComments = require('strip-json-comments')
+
 function registerPartials(handlebars) {
-	const partials = require('../../config/partials.json')
+	const partialsPath = path.join(__dirname, '../../config/partials.json')
+	if (!fs.existsSync(partialsPath)) {
+		return
+	}
+
+	const partials = JSON.parse(stripJsonComments(fs.readFileSync(partialsPath, 'utf8')))
 
 	// eslint-disable-next-line guard-for-in
 	for (const key in partials) {
